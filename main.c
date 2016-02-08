2cbe090f8c4b7c578c5a07471636f29b870b29ad
Francois SIDLER <fsidler@e2r11p21.42.fr>
2016-02-08 18:08:23 +0100

    norme

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: fsidler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 23:17:40 by fsidler           #+#    #+#             */
-/*   Updated: 2016/02/08 17:28:59 by fsidler          ###   ########.fr       */
+/*   Updated: 2016/02/08 18:07:55 by fsidler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/includes/libft.h"
+#include <errno.h>
 #include <stdio.h>//retirer
 #include "mlx.h"
 
@@ -49,17 +50,20 @@ int		main(int argc, char **argv)
 	}
 	if ((fd = open(argv[1], O_RDONLY)) == -1)
 	{
-		write(2, "error: No such file or directory\n", 33);
+		ft_putstr_fd(ft_strjoin("error: ", ft_strjoin(strerror(errno), "\n")), 2);
 		return (0);
 	}
 	while (get_next_line(fd, &line) > 0)
 		buf = ft_strjoin(ft_strjoin(buf, line), "\n");
-	ft_error(buf);
-	printf("%s\n", buf);
 	if (get_next_line(fd, &line) != 0)
+	{
 		write(2, "error: get_next_line returned -1\n", 32);
-	//appeller la fonction pour les erreurs
+		free(buf);
+		return (0);
+	}
+	if (!ft_error(buf))
+		printf("%s\n", buf);//retirer
 	if (close(fd) == -1)
-		write(2, "close error\n", 12);
+		ft_putstr_fd(ft_strjoin("error: ", ft_strjoin(strerror(errno), "\n")), 2);
 	return (fd);
 }
