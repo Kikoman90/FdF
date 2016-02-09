284436478da0023dc19945b1a40177230d20be2a
Francois SIDLER <fsidler@e2r12p13.42.fr>
2016-02-09 14:00:19 +0100

    norme

--- compil.sh
+++
@@ -1,3 +0,0 @@
-make -C libft/ fclean && make -C libft/
-clang -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
-clang -o FdF main.o -I libft/includes -L libft/ -lft -lmlx -framework OpenGL -framework AppKit
